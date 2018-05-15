aa8cf6634a1d01fa38b42d68b9434dfe563b102a
Edgar Gago Carrillo <edgar.gago.carrillo@avanade.com>
2018-05-15 10:07:48 +0200

    Update

--- FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -1,15 +1,10 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel; 
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FriendOrganizer.UI.ViewModel
 {
-
     public class MainViewModel: ViewModelBase
     {
         private IFriendDataService _friendDataService;
